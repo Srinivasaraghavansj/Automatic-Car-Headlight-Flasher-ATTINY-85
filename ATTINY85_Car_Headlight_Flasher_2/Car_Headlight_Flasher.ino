9c9bd190536862cdc2ea1f3217be273a271ab1bd
Srinivasaraghavansj <47287887+Srinivasaraghavansj@users.noreply.github.com>
2021-06-21 19:22:42 +0530

    Add files via upload

---
+++ ATTINY85_Car_Headlight_Flasher_2/Car_Headlight_Flasher.ino
@@ -0,0 +1,23 @@
+int releasepin=5, connectpin=6, switchpin=3;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(releasepin,OUTPUT);
+pinMode(connectpin,OUTPUT);
+pinMode(switchpin,INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(digitalRead(switchpin)==LOW)
+{
+digitalWrite(releasepin,HIGH);
+delay(20);
+digitalWrite(connectpin,HIGH);
+delay(700);
+digitalWrite(connectpin,LOW);
+delay(300);
+}
+else
+digitalWrite(releasepin, LOW);
+}
---
+++ ATTINY85_Headlight_Flasher/ATTINY85_Headlight_Flasher.ino
@@ -0,0 +1,23 @@
+int releasepin=3, connectpin=4, switchpin=2;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(releasepin,OUTPUT);
+pinMode(connectpin,OUTPUT);
+pinMode(switchpin,INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(digitalRead(switchpin)==LOW)
+{
+digitalWrite(releasepin,LOW);
+delay(20);
+digitalWrite(connectpin,LOW);
+delay(700);
+digitalWrite(connectpin,HIGH);
+delay(300);
+}
+else
+digitalWrite(releasepin, HIGH);
+}
